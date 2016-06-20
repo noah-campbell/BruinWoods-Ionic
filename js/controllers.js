c2766033cc6a75d10a12da9d45e5fc230fd15f48
noah-campbell <noahleecampbell@gmail.com>
2016-06-20 16:08:55 -0700

    Bruin Woods Ionic
    Bruin Woods Front End

---
+++ js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('page4Ctrl', function($scope) {
+
+})
+   
+.controller('page5Ctrl', function($scope) {
+
+})
+   
+.controller('page6Ctrl', function($scope) {
+
+})
+   
+.controller('page7Ctrl', function($scope) {
+
+})
+   
+.controller('contactUsCtrl', function($scope) {
+
+})
+ 
